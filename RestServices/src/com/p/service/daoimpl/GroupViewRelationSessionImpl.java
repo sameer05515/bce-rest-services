e55b9647cceafb4f0f305d3f24cbd88f0c36a815
premendrakumar <premendra.bce05515@gmail.com>
2016-10-21 13:10:25 +0530

    first phase commit for Topic-Group-View relations

---
+++ RestServices/src/com/p/service/daoimpl/GroupViewRelationSessionImpl.java
@@ -0,0 +1,25 @@
+package com.p.service.daoimpl;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import com.p.service.dao.GroupSession;
+import com.p.service.dao.GroupViewRelationSession;
+import com.p.service.exception.RestServiceException;
+import com.p.service.pojo.Group;
+import com.p.sevice.common.HibernateSessionFactory;
+
+public class GroupViewRelationSessionImpl implements GroupViewRelationSession {
+
+	private static final Logger logger = Logger.getLogger(GroupViewRelationSessionImpl.class.getName());
+
+	
+
+	
+}
---
+++ RestServices/src/com/p/service/daoimpl/ViewTopicRelationSessionImpl.java
@@ -0,0 +1,12 @@
+package com.p.service.daoimpl;
+
+import org.apache.log4j.Logger;
+
+import com.p.service.dao.ViewTopicRelationSession;
+
+public class ViewTopicRelationSessionImpl implements ViewTopicRelationSession {
+
+	private static final Logger logger = Logger.getLogger(ViewTopicRelationSessionImpl.class.getName());
+
+	
+}
